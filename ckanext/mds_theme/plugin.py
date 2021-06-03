db824e7c08efa55360813c75f75acb404f2771b7
Ema <mast3er@gmail.com>
2021-06-03 08:13:58 -0300

    custom fields trys

--- ckanext/mds_theme/plugin.py
+++ ckanext/mds_theme/plugin.py
@@ -35,9 +35,11 @@ def freq_to_text(update_freq):
     return upds[update_freq]
 
 
-class MdsThemePlugin(plugins.SingletonPlugin):
+class MdsThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IDatasetForm)
+
 
     # IConfigurer
 
@@ -52,3 +54,38 @@ class MdsThemePlugin(plugins.SingletonPlugin):
         '''
         return {'astr_to_dict': str_to_dict, 'freq_to_text': freq_to_text}
 
+    def _modify_package_schema(self, schema):
+        schema.update({
+            'custom_text': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def create_package_schema(self):
+        schema = super(MdsThemePlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(MdsThemePlugin, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+
+    def show_package_schema(self):
+        schema = super(MdsThemePlugin, self).show_package_schema()
+        schema.update({
+            'custom_text': [toolkit.get_converter('convert_from_extras'),
+                            toolkit.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
