c0fa0172517ccaed05dc860998da1a04f5776e79
baldosa <ema@guillen.com.ar>
2023-09-14 11:25:39 -0300

    check date before update

--- ckanext/mds_theme/datajson.py
+++ ckanext/mds_theme/datajson.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import os
 import json
 import ast
@@ -7,10 +7,10 @@ import requests
 from datetime import datetime
 from urllib.parse import urlparse
 
-from pydatajson import DataJson
-from pydatajson.writers import write_xlsx_catalog
+# from pydatajson import DataJson
+# from pydatajson.writers import write_xlsx_catalog
 
-SITE_URL = 'https://datosabiertos.desarrollosocial.gob.ar/'
+SITE_URL = "https://datosabiertos.desarrollosocial.gob.ar/"
 # SITE_URL = 'http://localhost:5000/'
 
 BASE_DICT = {
@@ -21,96 +21,103 @@ BASE_DICT = {
     "superThemeTaxonomy": "http://datos.gob.ar/superThemeTaxonomy.json",
     "publisher": {
         "mbox": "datosabiertos@desarrollosocial.gob.ar",
-        "name": "Ministerio de Desarrollo Social de la Nación"
+        "name": "Ministerio de Desarrollo Social de la Nación",
     },
     "issued": "",
     "modified": f'{datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")}',
-    "language": [
-        "spa"
-    ],
+    "language": ["spa"],
     "license": "cc-nc",
     "homepage": "https://datosabiertos.desarrollosocial.gob.ar/",
     "rights": "",
-    "spatial": [
-        "ARG"
-    ],
+    "spatial": ["ARG"],
     "themeTaxonomy": [],
 }
 
+
 def gen_datasetinfo(dataset_id):
-    dataset_url = f'{SITE_URL}/api/3/action/package_show?id={dataset_id}'
+    dataset_url = f"{SITE_URL}/api/3/action/package_show?id={dataset_id}"
     r = requests.get(dataset_url)
-    dataset_info = r.json()['result']
+    dataset_info = r.json()["result"]
     dataset = {
-        'title': dataset_info['title'],
-        'description': dataset_info['notes'],
-        'modified': dataset_info['metadata_modified'],
-        "identifier": dataset_info['id'],
-        "issued": dataset_info['metadata_created'],
-        "landingPage": dataset_info['url'],
-        "license": dataset_info['license_title'],
-        "spatial": [
-            "ARG"
-        ],
+        "title": dataset_info["title"],
+        "description": dataset_info["notes"],
+        "modified": dataset_info["metadata_modified"],
+        "identifier": dataset_info["id"],
+        "issued": dataset_info["metadata_created"],
+        "landingPage": dataset_info["url"],
+        "license": dataset_info["license_title"],
+        "spatial": ["ARG"],
         "publisher": {
-            "mbox": dataset_info['author_email'],
-            "name":  dataset_info['author']
+            "mbox": dataset_info["author_email"],
+            "name": dataset_info["author"],
         },
         "contactPoint": {
-            "fn": dataset_info['maintainer'],
-            "hasEmail": dataset_info['maintainer_email']
+            "fn": dataset_info["maintainer"],
+            "hasEmail": dataset_info["maintainer_email"],
         },
         "distribution": gen_resources(dataset_info),
-        "keyword": list(tag['name'] for tag in dataset_info['tags']),
-        "superTheme": dataset_info['super_theme'].replace('{','').replace('}','').split(','),
-        "accrualPeriodicity": dataset_info['update_frequency'],
-        "language": [
-            "spa"
-        ]
+        "keyword": list(tag["name"] for tag in dataset_info["tags"]),
+        "superTheme": dataset_info["super_theme"]
+        .replace("{", "")
+        .replace("}", "")
+        .split(","),
+        "accrualPeriodicity": dataset_info["update_frequency"],
+        "language": ["spa"],
     }
     return dataset
 
+
 def gen_resources(dataset_info):
-    resources = dataset_info['resources']
+    resources = dataset_info["resources"]
     datasets = []
     for r in resources:
         try:
-            fields = ast.literal_eval(r['file_fields_dict'])
+            fields = ast.literal_eval(r["file_fields_dict"])
         except:
             fields = []
-        
-        datasets.append({
-            "identifier": r['id'],
-            "format": r['format'],
-            "title": r['name'],
-            "description": r['description'],
-            "type": "file.upload",
-            "issued": r['created'],
-            "modified": r['last_modified'],
-            "license": "cc-by",
-            "accessURL": f"{SITE_URL}/dataset/{dataset_info['id']}/resource/{r['id']}",
-            "field": fields,
-            "downloadURL": r['url'],
-            "fileName": os.path.basename(urlparse(r['url']).path)
-        })
+
+        datasets.append(
+            {
+                "identifier": r["id"],
+                "format": r["format"],
+                "title": r["name"],
+                "description": r["description"],
+                "type": "file.upload",
+                "issued": r["created"],
+                "modified": r["last_modified"],
+                "license": "cc-by",
+                "accessURL": f"{SITE_URL}/dataset/{dataset_info['id']}/resource/{r['id']}",
+                "field": fields,
+                "downloadURL": r["url"],
+                "fileName": os.path.basename(urlparse(r["url"]).path),
+            }
+        )
 
     return datasets
 
-#%%
-if __name__ == '__main__':
-    datasets_url = f'{SITE_URL}/api/3/action/package_list'
+
+def latest_update():
+    response = requests.get(f"{SITE_URL}/data.json")
+    data = response.json()
+    return max([x["modified"] for x in data["dataset"]])
+
+
+# %%
+if __name__ == "__main__":
+    datasets_url = f"{SITE_URL}/api/3/action/package_list"
     r = requests.get(datasets_url)
-    datasets_id = r.json()['result']
+    datasets_id = r.json()["result"]
 
     datasets = []
     for d in datasets_id:
-        print(d)
         datasets.append(gen_datasetinfo(d))
 
+    BASE_DICT["dataset"] = datasets
 
-    BASE_DICT['dataset'] = datasets
-    with open(f'{os.path.abspath(os.path.dirname(__file__))}/public/data.json', 'w') as outfile:
-        json.dump(BASE_DICT, outfile, indent=4)
+    if max([x["modified"] for x in BASE_DICT["dataset"]]) > latest_update():
+        with open(
+            f"{os.path.abspath(os.path.dirname(__file__))}/public/data.json", "w"
+        ) as outfile:
+            json.dump(BASE_DICT, outfile, indent=4)
 
     # write_xlsx_catalog(BASE_DICT, f'{os.path.abspath(os.path.dirname(__file__))}/public/data.json')
-
